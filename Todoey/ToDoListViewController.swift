151b0a0f61f66a12ebf89ade0b9473f9d43cf009
faraz2079 <farazchahi2079@gmail.com>
2024-08-20 17:54:35 +0200

    first part complete

--- Todoey/ToDoListViewController.swift
+++ Todoey/ToDoListViewController.swift
@@ -1,12 +1,23 @@
 import UIKit
 
-class ViewController: UITableViewController {
+class ToDoListViewController: UITableViewController {
 
+    let itemArray = ["buy tomtao", "study german", "meditation"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
 
 }
 
